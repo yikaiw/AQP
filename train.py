e8d48221a73f9d517d2200b7b311eee74ebb6b8a
yikaiw <845002185@qq.com>
2018-05-05 16:55:20 +0800

    hour

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     for city in ['bj', 'ld']:
         url_reader = utils.UrlReader(duration, city=city)
         url_data = url_reader.read_with_time()
-        x_train_data = np.array([url_data['station'], url_data['day'], url_data['hour']]).T
+        x_train_data = np.array([url_data['station'], np.dot(url_data['day'], 24) + url_data['hour']]).T
         if city == 'bj':
             y_train_data = np.array([url_data['pm25'], url_data['pm10'], url_data['o3']]).T
         else:
@@ -24,12 +24,14 @@ if __name__ == '__main__':
                 station_list = np.concatenate((station_list, [station] * HOUR_NUM))
             day_delta_list = [url_data['day_delta'] + i] * HOUR_NUM * STATION_NUM[city]
             hour_list = list(range(HOUR_NUM)) * STATION_NUM[city]
-            x_predict_data = np.array([station_list, day_delta_list, hour_list]).T
+            x_data = np.array([station_list, day_delta_list, hour_list]).T
+            hour_list = list(range(HOUR_NUM)) * STATION_NUM[city] + np.dot(day_delta_list, 24)
+            x_predict_data = np.array([station_list, hour_list]).T
 
             regr_rf = RFR(max_depth=MAX_DEPTH, random_state=2)
             regr_rf.fit(x_train_data, y_train_data)
 
             y_rf = regr_rf.predict(x_predict_data)
             filename = 'sub.csv'
-            csv_saver = utils.CsvSaver(x_predict_data, y_rf, city, filename, day=i)
+            csv_saver = utils.CsvSaver(x_data, y_rf, city, filename, day=i)
             csv_saver.save()
